b8b1ecee010493b3f45c2aa8beca8c4149a69f0b
Ivan Pavlov <myr00t@mail.ru>
2020-04-12 12:33:19 +0300

    IOSAConstraint change

--- src/logic/IOSAConstraint.java
+++ src/logic/IOSAConstraint.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class IOSAConstraint {
 	public int criterionCount;
-	public double[] minValues;
-	public double[] maxValues;
+	public double[][] minValues;
+	public double[][] maxValues;
 	
-	public IOSAConstraint(int criterionCount, double[] mins, double[] maxes) {
+	public IOSAConstraint(int criterionCount, double[][] mins, double[][] maxes) {
 		this.criterionCount = criterionCount;
 		this.minValues = Arrays.copyOf(mins, mins.length);
 		this.maxValues = Arrays.copyOf(maxes, maxes.length);
